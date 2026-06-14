9fac71e9413883bb706b9bbb55adf94f3126bdaf
YumanKh <yumankhoufache607@gmail.com>
2026-06-14 13:10:25 -0500

    feat: Add Note, Password & User Models

---
+++ V3-ASP.NET-API/Models/Password.cs
@@ -0,0 +1,12 @@
+﻿namespace SecureVaultAPI.Models
+{
+    public class Password
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
