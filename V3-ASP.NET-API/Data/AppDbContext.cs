3a4cd193ef4dd95daf3f2f386edc826b40967184
YumanKh <yumankhoufache607@gmail.com>
2026-06-14 13:14:35 -0500

    feat: Add AppDbContext

---
+++ V3-ASP.NET-API/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecureVaultAPI.Models;
+
+namespace NotesAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Password> Passwords { get; set; }
+    }
+}
