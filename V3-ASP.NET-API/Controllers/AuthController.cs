ff7d9518afc9f30b77aae51bef4638ffdda1d576
YumanKh <yumankhoufache607@gmail.com>
2026-06-15 16:49:52 -0500

    feat: add AuthController with register and login endpoints

---
+++ V3-ASP.NET-API/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecureVaultAPI.Services;
+using SecureVaultAPI.Models;
+
+namespace SecureVaultAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _service;
+
+        public AuthController(AuthService service) { _service = service; }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] AuthRegisterRequest model)
+        {
+            try
+            {
+                var user = await _service.RegisterAsync(model);
+                return Ok(new { Id = user.Id, UserName = user.UserName });
+            }
+            catch (Exception)
+            {
+                return BadRequest("An error occurred while registering the user.");
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] AuthLoginRequest model)
+        {
+            try
+            {
+                var token = await _service.LoginAsync(model);
+                return Ok(new { Token = token });
+            }
+            catch (Exception)
+            {
+                return BadRequest("An error occurred while logging in.");
+            }
+        }
+
+    }
+}
